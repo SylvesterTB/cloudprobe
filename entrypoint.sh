93931deed7aad00b0df4a6ac5e8fa521681e015e
sbroich_ncstate <sylvester.broich@gmail.com>
2025-12-16 17:08:52 -0500

    pathing

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ aws s3 ls s3://cloudprobe-dashboard
 ls -l output/results.json
 aws s3 cp output/results.json s3://cloudprobe-dashboard/results.json
 
-python -m cloudprobe.cli --config sample-test.json --no-exit-on-fail
+python -m cloudprobe.cli --config config/sample-test.json --no-exit-on-fail
 
 
 
