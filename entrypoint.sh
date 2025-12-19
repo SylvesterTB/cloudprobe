64fb703d383c5a84c047f9e5a55c22f713877822
sbroich_ncstate <sylvester.broich@gmail.com>
2025-12-19 11:03:31 -0500

    entrypoint restructure to avoid usage of old data + data not found error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
-aws s3 ls s3://cloudprobe-dashboard
-
-ls -l output/results.json
-aws s3 cp output/results.json s3://cloudprobe-dashboard/results.json
 
 python -m cloudprobe.cli --config config/sample-test.json --no-exit-on-fail
+
+ls -l output/results.json
+
+
+aws s3 ls s3://cloudprobe-dashboard
+
+aws s3 cp output/results.json s3://cloudprobe-dashboard/results.json
+
 
 
 
